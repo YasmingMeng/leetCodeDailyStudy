0d9d5402f92bbc7539e965eb2cb6469c0ed83b6f
mengyixue <1661237617@qq.com>
2022-06-14 01:20:21 +0800

    双栈数组实现队列解法1

---
+++ 剑指offer/段式回文.ts
@@ -0,0 +1,11 @@
+
+/**
+ * @Description: Longest Chunked Palindrome Decomposition
+ * 你会得到一个字符串 text 。你应该把它分成 k 个子字符串 (subtext1, subtext2，…， subtextk) ，要求满足:
+ * subtexti 是非空字符串
+ * 所有子字符串的连接等于 text ( 即subtext1 + subtext2 + ... + subtextk == text )
+ * subtexti == subtextk - i + 1 表示所有 i 的有效值( 即 1 <= i <= k )
+ * 返回k可能最大值。
+ * @Date: 2022-06-13 22:41:25
+ * @LastEditTime: 2022-06-13 22:43:43
+ */
